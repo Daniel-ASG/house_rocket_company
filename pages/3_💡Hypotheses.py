fa536e32084cf1879ac0c27f9fc6e55fccfb8c50
Daniel Gomes <63655512+Daniel-ASG@users.noreply.github.com>
2023-09-16 08:41:40 -0400

    Update 3_💡Hypotheses.py
    insertion of the line "import numpy as np " to correct its absence in the scrpit

--- pages/3_💡Hypotheses.py
+++ pages/3_💡Hypotheses.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot    as plt
 import streamlit            as st
 import pandas               as pd
 import seaborn              as sns
+import numpy                as np
 import geopandas
 
 from urllib.error         import HTTPError
